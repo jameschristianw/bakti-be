dddae6a153cafe5e48b00933ec4c78ed82220632
James Christian Wira <jamesch888@gmail.com>
2026-01-29 02:27:32 +0700

    fix: add boot logic

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,20 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if (config('app.env') === 'production' || config('app.env') === 'staging') {
+            URL::forceScheme('https');
+        }
+        Request::setTrustedProxies(
+            ['*'], // trust all proxies
+            Request::HEADER_X_FORWARDED_FOR
+            | Request::HEADER_X_FORWARDED_HOST
+            | Request::HEADER_X_FORWARDED_PORT
+            | Request::HEADER_X_FORWARDED_PROTO
+        );
+
+        // Set date/time localization for Carbon & Date facade
+        $locale = config('app.locale');
+        Carbon::setLocale($locale);
+        Date::setLocale($locale);
     }
 }
